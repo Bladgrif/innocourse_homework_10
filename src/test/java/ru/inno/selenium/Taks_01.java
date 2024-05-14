e92d9c87ec6aed56deacfe9185573c965077adc2
Badgrif <aswd@asd.com>
2024-05-14 23:41:38 +0300

    It works on my computer

--- src/test/java/ru/inno/selenium/Taks_01.java
+++ src/test/java/ru/inno/selenium/Taks_01.java
@@ -1,6 +1,7 @@
 package ru.inno.selenium;
 
 import com.codeborne.selenide.Configuration;
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
@@ -16,21 +17,24 @@ public class Taks_01 {
 //        WebDriver driver = new ChromeDriver();
 //        driver.get("https://the-internet.herokuapp.com/javascript_alerts");
         // WebDriver
-//        WebDriver browser = new ChromeDriver();
-//        browser.get("https://habr.ru");
+        WebDriverManager.chromedriver().browserVersion("114").setup();
+        WebDriver browser = new ChromeDriver();
+        browser.get("https://habr.ru");
+        browser.quit();
 //        // WebElement
 //        browser.quit();
-        Configuration.browserVersion = System.getProperty("browserVersion", "100.0");
-        open("https://habr.ru");
-        sleep(1000);
+//        Configuration.browserVersion = System.getProperty("browserVersion", "100.0");
+//        open("https://habr.ru");
+//        sleep(1000);
     }
 
     @Test
     public void userCanLoginByUsername() {
-        open("/login");
-        $(By.name("user.name")).setValue("johny");
-        $("#submit").click();
-        $(".loading_progress").should(disappear); // Само подождёт, пока элемент исчезнет
-        $("#username").shouldHave(text("Hello, Johny!")); // Само подождёт, пока у элемента появится нужный текст
+//        WebDriverManager.chromedriver().browserVersion("114").setup();
+//        open("/login");
+//        $(By.name("user.name")).setValue("johny");
+//        $("#submit").click();
+//        $(".loading_progress").should(disappear); // Само подождёт, пока элемент исчезнет
+//        $("#username").shouldHave(text("Hello, Johny!")); // Само подождёт, пока у элемента появится нужный текст
     }
 }
