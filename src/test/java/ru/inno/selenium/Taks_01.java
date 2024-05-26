0884199bf4797efd7097f31688386a08e6cdba9e
Badgrif <aswd@asd.com>
2024-05-26 12:50:58 +0300

    sdfg

--- src/test/java/ru/inno/selenium/Taks_01.java
+++ src/test/java/ru/inno/selenium/Taks_01.java
@@ -14,27 +14,7 @@ import static com.codeborne.selenide.Selenide.*;
 public class Taks_01 {
     @Test
     void sucessfullAuth() {
-//        WebDriver driver = new ChromeDriver();
-//        driver.get("https://the-internet.herokuapp.com/javascript_alerts");
-        // WebDriver
-//        WebDriverManager.chromedriver().browserVersion("114").setup();
-//        WebDriver browser = new ChromeDriver();
-//        browser.get("https://habr.ru");
-//        browser.quit();
-//        // WebElement
-//        browser.quit();
-//        Configuration.browserVersion = System.getProperty("browserVersion", "100.0");
-//        open("https://habr.ru");
-//        sleep(1000);
-    }
-
-    @Test
-    public void userCanLoginByUsername() {
-//        WebDriverManager.chromedriver().browserVersion("114").setup();
-        open("/login");
-        $(By.name("user.name")).setValue("johny");
-        $("#submit").click();
-        $(".loading_progress").should(disappear); // Само подождёт, пока элемент исчезнет
-        $("#username").shouldHave(text("Hello, Johny!")); // Само подождёт, пока у элемента появится нужный текст
+        open("https://habr.ru");
+        sleep(1000);
     }
 }
